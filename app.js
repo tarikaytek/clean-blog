d11cb52c037e813366bffa6eb28abd10882075b7
tarikaytek <aytek.tarik1@gmail.com>
2024-07-16 00:37:15 +0300

    configure views

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
 //template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //MIDDLEWARES
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+/* app.get('/', (req, res) => {
 
     const photo = {
         id: 1,
@@ -16,9 +32,9 @@ app.get('/', (req, res) => {
         description: "blog desc 1",
     }
     res.send(photo);
-});
+}); */
 
 const port = 5000;
 app.listen(port, () => {
-    console.log(`Sunucu ${port} adresinde baslatildi.`);
+  console.log(`Sunucu ${port} adresinde baslatildi.`);
 });
